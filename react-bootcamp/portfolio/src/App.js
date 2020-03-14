60a0e009ec5f65f5a9ce15267291d9fa2fe6a8d8
Pradipta Shrestha <pradipta@Pradiptas-MBP.home>
2020-03-14 16:43:59 -0500

    learn state

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -1,22 +1,27 @@
 import React, {Component} from 'react';
 
-class RegularClass{}
-
-class ComponentClass extends Component{}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-console.log('regularClassInstance', regularClassInstance);
-
-console.log('componentClassInstance', componentClassInstance);
-
 class App extends Component {
+    constructor(){
+        super();
+        this.state = { displayAbout: true };
+    }
+
     render() {
+        let about = (<div>
+                    <p>This app is created by Cowtown Software.</p>
+                    <p>We created this app in 2020.</p>
+                </div>);
+
+        if (!this.state.displayAbout){
+            about = null;
+        }
+        
         return (
             <div>
                 <h1>Welcome to My App</h1>
                 <p>lorem ipsum dorem ipsum</p>
+                <p>Thank you for using my app.</p>
+                {about}
             </div>
         )
     }
