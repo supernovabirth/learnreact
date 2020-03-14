0fc0f001428cea1cc20b314ed831b4ea3a9f26b0
Pradipta Shrestha <pradipta@Pradiptas-MBP.home>
2020-03-14 16:50:31 -0500

    inline within jsx if used just once

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -7,17 +7,18 @@ class App extends Component {
     }
 
     render() {
-        const about = this.state.displayAbout ? (<div>
-                    <p>This app is created by Cowtown Software.</p>
-                    <p>We created this app in 2020.</p>
-                </div>) : null;
         
         return (
             <div>
                 <h1>Welcome to My App</h1>
                 <p>lorem ipsum dorem ipsum</p>
                 <p>Thank you for using my app.</p>
-                {about}
+                {
+                this.state.displayAbout ? (<div>
+                    <p>This app is created by Cowtown Software.</p>
+                    <p>We created this app in 2020.</p>
+                </div>) : null
+                }
             </div>
         )
     }
